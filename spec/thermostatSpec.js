5e62ee5a0ca31cfa354ab8deda74f62e09cf193d
Anna <anna.hollandsmith3@gmail.com>
2016-06-22 16:00:08 +0100

    added test for minimum temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,17 +8,29 @@ var thermostat;
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function(){
-    expect(thermostat.getTemperature()).toEqual(20);
+  describe('adjust temperature', function(){
+    it('starts at 20 degrees', function(){
+      expect(thermostat.getTemperature()).toEqual(20);
+    });
+
+    it('can increase temperature by pressing up', function(){
+    	thermostat.incTemperature();
+    	expect(thermostat.getTemperature()).toEqual(21);
+    });
+
+    it('can decrease temperture by presssing down', function() {
+    	thermostat.decTemperature();
+    	expect(thermostat.getTemperature()).toEqual(19);
+    });
   });
 
-  it('can increase temperature by pressing up', function(){
-  	thermostat.incTemperature();
-  	expect(thermostat.getTemperature()).toEqual(21);
-  });
 
-  it('can decrease temperture by presssing down', function() {
-  	thermostat.decTemperature();
-  	expect(thermostat.getTemperature()).toEqual(19);
+  describe('', function(){
+    it('has a minimum temperature of 10', function(){
+      for ( var i = 0; i < 10; i++ ) {
+        thermostat.decTemperature();
+      }
+      expect(thermostat.getTemperature()).toEqual(10);
+    });
   });
 });
